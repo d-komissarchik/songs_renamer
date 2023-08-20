72553888d7f4380e2bea9f945eb0cba7a3de87b4
d-komissarchik <0667669609@ukr.net>
2023-08-20 11:58:16 +0300

    added func rename

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 
-DIRECTORY = r''#путь к папке
+DIRECTORY = r''  # путь к папке
 
 
 def files(dir):
@@ -8,15 +8,17 @@ def files(dir):
         for name in files:
             rename(root, name)
 
+
 def rename(root, name):
     valid_name = get_valid_name(name)
     old_file = os.path.join(root, name)  # получаем путь к старому файлу соединяя ссылку с названием файла
     new_file = os.path.join(root, valid_name)
     os.rename(old_file, new_file)  # переименовываем, указывая старый и потом новый путь
 
-def get_valid_name(name):
-    pass
 
+def get_valid_name(name):
+
+    return name
 
 
 if __name__ == '__main__':
